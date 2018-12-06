88041ee80a2be236b706503b56c8007fb9701075
Akshay Gupta <akshaygrkesh@gmail.com>
2018-12-06 11:38:55 +0530

    Parse/Encode body refactoring into object

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const util = require('util')
 
 const readFile = util.promisify(fs.readFile)
 const protocol = 'HTTP/1.1'
-const contentTypes = {
+const fileTypes = {
   '.txt': 'text/plain',
   '.html': 'text/html',
   '.css': 'text/css',
@@ -12,6 +12,11 @@ const contentTypes = {
   '.js': 'application/javascript',
   '.png': 'image/png',
   '.jpeg': 'image/jpeg'
+}
+const contentTypes = {
+  'text/plain': { 'decode': String, 'encode': String },
+  'application/json': { 'decode': JSON.parse, 'encode': JSON.stringify },
+  'application/x-www-form-urlencoded': { 'decode': decodeURI, 'encode': encodeURI }
 }
 const statusTypes = {
   'ok': '200 OK',
@@ -44,11 +49,11 @@ function handleConnection (socket) {
 function parseRequestLine (requestLine) {
   let info = requestLine[0].split(' ')
   if (info.length !== 3) {
-    throw Error()
+    throw Error(requestLine, info)
   } return { 'method': info[0], 'target': info[1], 'protocol': info[2] }
 }
 
-function parseReqHeader (request) {
+function parseRequestHeader (request) {
   let header = { }
   let headerField
 
@@ -61,23 +66,19 @@ function parseReqHeader (request) {
   }
 }
 
-function parseReqBody (body, type) {
-  if (!type) {
+function parseRequestBody (body, type) {
+  try {
+    return contentTypes[type].decode(body)
+  } catch (err) {
     return body
-  } if (type === 'text/plain') {
-    return String(body)
-  } if (type === 'application/json') {
-    return JSON.parse(body)
-  } if (type === 'application/x-www-form-urlencoded') {
-    return decodeURI(body)
-  } // form data
+  }
 }
 
 function parseRequest (request) {
   let lines = request.split('\r\n')
   let reqLine = parseRequestLine(lines.splice(0, 1))
-  let [header, body] = parseReqHeader(lines)
-  body = parseReqBody(body[0], header['Content-Type'])
+  let [header, body] = parseRequestHeader(lines)
+  body = parseRequestBody(body[0], header['Content-Type'])
   return { 'reqLine': reqLine, 'header': header, 'body': body }
 }
 
@@ -106,13 +107,12 @@ function generateResponseHeader (type, length) {
 function encodeBody (body, type) {
   if (body.length === 0) {
     return ''
-  } if (type === 'text/plain') {
-    return String(body)
-  } if (type === 'application/json') {
-    return JSON.stringify(body)
-  } if (type === 'application/x-www-form-urlencoded') {
-    return encodeURI(body)
-  } return body // or return ''
+  }
+  try {
+    return contentTypes[type].encode(body)
+  } catch (err) {
+    return body // or return ''
+  }
 }
 
 function generateResponse (body, type, status) {
@@ -134,7 +134,7 @@ async function staticFileHandler (request, handlers) {
   target = `public${target}` // make variable
   try {
     let body = await readFile(target)
-    let type = contentTypes[target.match(/[.](\w)+/)[0]]
+    let type = fileTypes[target.match(/[.](\w)+/)[0]]
     return generateResponse(body, type, 'ok')
   } catch (err) {
     return handlers.next().value(request, handlers)
@@ -166,7 +166,7 @@ async function requestHandler (request) {
   try {
     request = parseRequest(request)
   } catch (err) {
-    console.log('Error', err)
+    console.log('Request is\n', request) // debug
     return generateResponse('', '', 'badreq') // bad request error
   }
   if (!Object.keys(routes).includes(request.reqLine.method)) { // not implemented error
@@ -178,7 +178,7 @@ async function requestHandler (request) {
 
 async function processRequest (request, socket) {
   let response = await requestHandler(request)
-  console.log(String(response)) // debug
+  // console.log(String(response)) // debug
   socket.write(response)
 }
 //
