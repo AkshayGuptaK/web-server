8206fe26bc102c9f461934c30f7b7f12887c3e53
Akshay Gupta <akshaygrkesh@gmail.com>
2018-12-07 12:41:45 +0530

    Added support for chunked requests

--- api.js
+++ api.js
@@ -20,10 +20,96 @@ exports.addRoute = function (method, route, controller) {
 
 function handleConnection (socket) {
   console.log('Connection made')
+  let request = { 'reqLine': '', 'header': {}, 'body': '' }
   socket.on('error', (err) => console.log(err))
   socket.setEncoding('utf8')
-  socket.on('data', data => processRequest(data, socket))
+  socket.on('data', data => processData(data, request, socket))
 }
+
+function processData (data, request, socket) {
+  let needed = request.header['content-length']
+  if (needed !== undefined) {
+    if (request.header.done) {
+      request.body = request.body.concat(data)
+      checkContentLength(needed, request, socket)
+    } else {
+      let lines = data.split('\r\n')
+      let body = setHeader(lines, request)
+      checkBody(needed, body, request, socket)
+    }
+  } else {
+    let lines = data.split('\r\n')
+    if (request.reqLine !== '') {
+      if (request.header.done) {
+        clearRequest(request)
+        socket.write(generateResponse('', '', 'badreq'))
+      } else {
+        let body = setHeader(lines, request)
+        needed = request.header['content-length']
+        if (request.header.done) {
+          checkBody(needed, body, request, socket)
+        }
+      }
+    } else { // executed if no req line i.e. fresh request
+      request['reqLine'] = parseRequestLine(lines.splice(0, 1))
+      let body = setHeader(lines, request)
+      needed = request.header['content-length']
+      if (request.header.done) {
+        checkBody(needed, body, request, socket)
+      }
+    }
+  }
+}
+
+function checkContentLength (needed, request, socket) {
+  let length = Buffer.byteLength(request.body)
+  needed = (needed === undefined) ? 0 : Number(needed)
+  if (needed === length) {
+    submitRequest(request, socket)
+  } else if (needed < length) {
+    clearRequest(request)
+    socket.write(generateResponse('', '', 'badreq'))
+  }
+}
+
+function checkBody (needed, body, request, socket) {
+  body = body.join('\r\n')
+  if (body) {
+    request.body = request.body.concat(body)
+    checkContentLength(needed, request, socket)
+  } else if (request.header.done) {
+    submitRequest(request, socket)
+  }
+}
+
+function setHeader (lines, request) {
+  let [header, body] = parseRequestHeader(lines)
+  Object.assign(request.header, header)
+  return body
+}
+
+function submitRequest (request, socket) {
+  request.body = parseRequestBody(request.body, request.header['content-type'])
+  let req = {}
+  Object.assign(req, request)
+  processRequest(req, socket)
+  clearRequest(request)
+}
+
+function clearRequest (request) {
+  request.reqLine = ''
+  request.header = {}
+  request.body = ''
+}
+
+// function parseRequest (request) {
+//   console.log('Request is\n', request)// debug
+//   let lines = request.split('\r\n')
+//   let reqLine = parseRequestLine(lines.splice(0, 1))
+//   let [header, body] = parseRequestHeader(lines)
+//   body = parseRequestBody(body, header['content-type'])
+//   return { 'reqLine': reqLine, 'header': header, 'body': body }
+// }
 
 function parseRequestLine (requestLine) {
   let info = requestLine[0].split(' ')
@@ -38,27 +124,20 @@ function parseRequestHeader (request) {
 
   for (let i = 0; i < request.length; i++) {
     if (!request[i]) {
+      header.done = true
       return [header, request.splice(i + 1)]
     }
     headerField = request[i].split(/:(.+)/)
-    header[headerField[0]] = headerField[1].trim()
-  }
+    header[headerField[0].toLowerCase()] = headerField[1].trim()
+  } return [header, '']
 }
 
 function parseRequestBody (body, type) {
   try {
-    return contentTypes[type].decode(body)
+    return config.contentTypes[type].decode(body)
   } catch (err) {
     return body
   }
-}
-
-function parseRequest (request) {
-  let lines = request.split('\r\n')
-  let reqLine = parseRequestLine(lines.splice(0, 1))
-  let [header, body] = parseRequestHeader(lines)
-  body = parseRequestBody(body[0], header['Content-Type'])
-  return { 'reqLine': reqLine, 'header': header, 'body': body }
 }
 
 function utcDate () {
@@ -67,8 +146,8 @@ function utcDate () {
 }
 
 function generateStatusLine (status) {
-  status = statusTypes[status]
-  return Buffer.from(`${protocol} ${status}\r\n`)
+  status = config.statusTypes[status]
+  return Buffer.from(`${config.protocol} ${status}\r\n`)
 }
 
 function generateResponseHeader (type, length) {
@@ -88,9 +167,9 @@ function encodeBody (body, type) {
     return ''
   }
   try {
-    return contentTypes[type].encode(body)
+    return config.contentTypes[type].encode(body)
   } catch (err) {
-    return body // or return ''
+    return body
   }
 }
 
@@ -113,7 +192,7 @@ async function staticFileHandler (request, handlers) {
   target = `public${target}` // make variable
   try {
     let body = await readFile(target)
-    let type = fileTypes[target.match(/[.](\w)+/)[0]]
+    let type = config.fileTypes[target.match(/[.](\w)+/)[0]]
     return generateResponse(body, type, 'ok')
   } catch (err) {
     return handlers.next().value(request, handlers)
@@ -123,7 +202,7 @@ async function staticFileHandler (request, handlers) {
 async function routeHandler (request, handlers) {
   try {
     let body = await routes[request.reqLine.method][request.reqLine.target](request)
-    let type = request.header['Content-Type']
+    let type = request.header['content-type']
     body = Buffer.from(encodeBody(body, type))
     return generateResponse(body, type, 'ok')
   } catch (err) {
@@ -142,13 +221,12 @@ function * genHandlers () {
 }
 
 async function requestHandler (request) {
-  try {
-    request = parseRequest(request)
-  } catch (err) {
-    console.log('Request is\n', request) // debug
-    return generateResponse('', '', 'badreq') // bad request error
-  }
-  if (!Object.keys(routes).includes(request.reqLine.method)) { // not implemented error
+  // try {
+  //   request = parseRequest(request)
+  // } catch (err) {
+  //   return generateResponse('', '', 'badreq') // bad request error
+  // }
+  if (!Object.keys(routes).includes(request.reqLine.method)) { // method not implemented error
     return generateResponse('', '', 'notimp')
   }
   let handlers = genHandlers()
@@ -157,6 +235,6 @@ async function requestHandler (request) {
 
 async function processRequest (request, socket) {
   let response = await requestHandler(request)
-  console.log(String(response)) // debug
+  // console.log(String(response)) // debug
   socket.write(response)
 }
--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const api = require('./api')
 
 const server = api.createServer()
 api.listen(server, 8000)
-// addRoute('GET', '/tgt', req => 'random text')
+api.addRoute('GET', '/tgt', req => 'random text')
 api.addRoute('POST', '/echo', req => req.body)
